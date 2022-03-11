acfb7ea9c96f630b14b87afa5b3472b3a393af5e
cioffstix <joel.cioffi@gmail.com>
2022-03-11 15:21:29 -0500

    worked on tillemap

--- main.ts
+++ main.ts
@@ -23,24 +23,7 @@ let item: Sprite = null
 let touching_ground = false
 let Jumper: Sprite = null
 tiles.setTilemap(tilemap`level1`)
-Jumper = sprites.create(img`
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . . . . b c . . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 5 d f . . 
-    . . . . b 5 5 1 f f 5 d 4 c . . 
-    . . . . b 5 5 d f b d d 4 4 . . 
-    b d d d b b d 5 5 5 4 4 4 4 4 b 
-    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
-    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
-    c d d c d 5 5 b 5 5 5 5 5 5 b . 
-    c b d d c c b 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
-    `, SpriteKind.Player)
+Jumper = sprites.create(assets.image`Ducky`, SpriteKind.Player)
 tiles.placeOnTile(Jumper, tiles.getTileLocation(0, 14))
 scene.cameraFollowSprite(Jumper)
 Jumper.ay = 150
